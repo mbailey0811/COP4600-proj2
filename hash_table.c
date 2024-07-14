41d6626b8f8c4ad90748377974da0253ac9e14f6
Megan Bailey <mbailey0811@yahoo.com>
2024-07-13 23:49:08 -0400

    adding insert, delete, and search functions

---
+++ hash_table.c
@@ -0,0 +1,147 @@
+#include "hash_table.h"
+#include "locks.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//head of the linked list for the hash table
+static hashRecord *head = NULL;
+
+//using the Jenkin's one at a time hash function as given
+uint32_t hash(const uint8_t* key, size_t length) {
+    size_t i = 0;
+    uint32_t hash = 0;
+    while (i != length) {
+        hash += key[i++];
+        hash += hash << 10;
+        hash ^= hash >> 6;
+    }
+    hash += hash << 3;
+    hash ^= hash >> 11;
+    hash += hash << 15;
+    return hash;
+  
+}
+
+void insert(const char *key, uint32_t value) {
+    //calculate hash
+    uint32_t hashValue = hash(key, strlen(key));   
+
+    //acquire lock
+    write_lock();
+
+    hashRecord* temp = head;
+    hashRecord* previous = NULL;
+
+    //look for node and update salary if already in list
+    while (temp != NULL) {
+        if (temp->hash == hashValue && strcmp(temp->name, key) == 0) {
+            temp->salary = value;
+            write_unlock();
+            return;
+        }
+        //move to next record
+        previous = temp;
+        temp = temp->next;
+    }
+
+    //if node isn't already in list, create new node
+    hashRecord* newEntry = (hashRecord *)malloc(sizeof(hashRecord));
+    
+    //null pointer check
+    if (!newEntry) {
+        printf("NULL POINTER, UNABLE TO INSERT");
+        return;
+    } 
+
+    //fill in the struct for the new entry
+    newEntry->hash = hashValue;
+    strcpy(newEntry->name, key, 50);
+    newEntry->salary = value;
+    newEntry->next = NULL;
+
+    //previous should point to the end of the table,
+    //so we just add the new entry next.
+    //if previous doesn't, this must be the new head
+    if (previous == NULL) {
+        head = newEntry;
+    } else {
+        previous->next = newEntry
+    }
+
+    //release the write lock
+    write_unlock();
+}
+
+void delete(const char *key) {
+    //calculate hash
+    uint32_t hashValue = hash(key, strlen(key));   
+
+    //acquire lock
+    write_lock();
+
+    hashRecord* temp = head;
+    hashRecord* previous = NULL;
+
+    //look for node and update salary if already in list
+    while (temp != NULL) {
+        if (temp->hash == hashValue && strcmp(temp->name, key) == 0) {
+            //node has been found,
+            //if it is first in the list then assign a new head
+            if (previous == NULL) {
+                head = temp->next;
+            } else {
+                //otherwise, just skip this node in the list
+                previous->next = current->next;
+            }
+            //free the memory and exit loop
+            free(temp);
+            break;
+        }
+        //move to next record
+        previous = temp;
+        temp = temp->next;
+    }
+    write_unlock();
+}
+
+hashRecord* search(const char *key) {
+    //calculate hash
+    uint32_t hashValue = hash(key, strlen(key));   
+
+    //acquire lock
+    read_lock();
+
+    hashRecord* temp = head;
+
+    //look for node
+    while (temp != NULL) {
+        if (temp->hash == hashValue && strcmp(temp->name, key) == 0) {
+            //node has been found
+            read_unlock();
+            return temp;
+        }
+        //move to next record
+        temp = temp->next;
+    }
+
+    read_unlock();
+    return NULL;
+
+}
+
+This function searches for a
+ key-data pair in the hash table
+  and returns the value, if it
+   exists. To search for a key-data
+    pair, the function first computes 
+    the hash value of the key acquires
+     a reader lock. Then, it searches 
+     the linked list for the key. If 
+     the key is found, it returns the 
+     value. Otherwise, it returns NULL. 
+     Finally, it releases the read lock 
+     and returns. The caller should then 
+     print the record or "No Record Found" 
+     if the return is NULL.
